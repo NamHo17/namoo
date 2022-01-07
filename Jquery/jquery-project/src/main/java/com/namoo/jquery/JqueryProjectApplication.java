2e468718c827ca4a52cde504144099b61d45ddf7
knh <rlaskagh776@gmail.com>
2022-01-07 11:01:21 +0900

    Jquery Project commit

---
+++ Jquery/jquery-project/src/main/java/com/namoo/jquery/JqueryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.namoo.jquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqueryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JqueryProjectApplication.class, args);
+    }
+
+}
---
+++ Jquery/jquery-project/src/test/java/com/namoo/jquery/JqueryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namoo.jquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JqueryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
